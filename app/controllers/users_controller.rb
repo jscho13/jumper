efa395407ba8499d419495fef6aa8b6b6f7259f4
Joseph Cho <jscho13@gmail.com>
2016-01-04 16:14:46 -0500

    add user routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,14 @@ class UsersController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
 
   def index
 
   end
 
   def destroy
-    redirect_to '/'
+
   end
 
 end
