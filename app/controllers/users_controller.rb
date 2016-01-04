83e3da7086ee1e636e37131d4928e1539a30c7e1
Joseph Cho <jscho13@gmail.com>
2016-01-04 15:37:44 -0500

    sign out issues

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  def index
+
+  end
+
+  def destroy
+    redirect '/'
+  end
+
+end
