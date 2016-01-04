17bcfbbb027e1a346ca891c2f429799793d5552c
sallypikachu <sallypikachu@gmail.com>
2016-01-04 17:13:06 -0500

    Created activities model, migration and index

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160104213930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "activities", force: :cascade do |t|
+    t.string   "activity_name", null: false
+    t.text     "description"
+    t.integer  "venue_id"
+    t.integer  "price_range"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "activities", ["activity_name", "venue_id"], name: "index_activities_on_activity_name_and_venue_id", unique: true, using: :btree
 
 end
