5c3eac93e1d32586471ce1615e10d37942618c3c
sallypikachu <sallypikachu@gmail.com>
2016-01-05 14:00:58 -0500

    use associated factories, sequece email

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -3,21 +3,21 @@ FactoryGirl.find_definitions
 
 FactoryGirl.define do
   factory :user do
-    email {Faker::Internet.email}
+    sequence(:email) { |n| "faketestuser#{n}@example.com" }
+    # email {Faker::Internet.email}
     password {Faker::Internet.password}
   end
 
   factory :activity do
     activity_name {Faker::Hipster.word}
     description {Faker::Hipster.paragraph}
-    sequence(:venue_id) { |n| "#{n}" }
     price_range "2"
   end
 
   factory :review do
     rating 5
     review_body {Faker::Hipster.paragraph}
-    activity_id 1
-    user_id 1
+    activity
+    user
   end
 end
