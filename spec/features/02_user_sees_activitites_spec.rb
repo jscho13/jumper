ab57b319af7e55ed61c3f10812ca288bc2c04423
sallypikachu <sallypikachu@gmail.com>
2016-01-05 13:48:40 -0500

    Fixing Hound comments

--- spec/features/02_user_sees_activitites_spec.rb
+++ spec/features/02_user_sees_activitites_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 # [] Visiting the `/activities` path should contain a list of restaurants.
-# [] Visiting the `/activities/10 path should show the details for a activity with the ID of 10.
+# [] Visiting the `/activities/10 path should show the details for a activity
+#    with the ID of 10.
 # [] Visiting the root path should display a list of all activities.
 
 feature "user sees a list of his/her activities" do
   before(:each) do
-    @bodaborg = Activity.create(activity_name: "BodaBorg",    description: "20 challenging mental and physical courses", venue_id: 1, price_range: 2)
-
-    @skyzone = Activity.create(activity_name: "Trampoline Dodgeball", description: "Exactly as described", venue_id: 2, price_range: 1)
+    @bodaborg = FactoryGirl.create(:activity)
+    @skyzone = FactoryGirl.create(:activity)
   end
 
   scenario "sees a list of activities and link for new activity" do
@@ -25,7 +25,7 @@ feature "user sees a list of his/her activities" do
   scenario "clicks link and is taken to show page for given activity" do
     visit activities_path
 
-    click_link "BodaBorg"
+    click_link @bodaborg.activity_name
 
     expect(page).to have_content @bodaborg.activity_name
     expect(page).to have_content @bodaborg.description
@@ -37,7 +37,7 @@ feature "user sees a list of his/her activities" do
     visit new_activity_path
 
     fill_in "Activity Name", with: "Bouldering"
-    fill_in "Description", with: "Climb 25 feet high wall with only your bare hands"
+    fill_in "Description", with: "Climb 25 feet wall with only your bare hands"
     fill_in "Venue", with: "Central Rock Gym"
     fill_in "Price Range", with: "$$"
 
--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -1,8 +1,23 @@
+require 'faker'
 FactoryGirl.find_definitions
 
 FactoryGirl.define do
   factory :user do
-    email "a@gmail.com"
-    password "applesTV"
+    email {Faker::Internet.email}
+    password {Faker::Internet.password}
+  end
+
+  factory :activity do
+    activity_name {Faker::Hipster.word}
+    description {Faker::Hipster.paragraph}
+    sequence(:venue_id) { |n| "#{n}" }
+    price_range "2"
+  end
+
+  factory :review do
+    rating 5
+    review_body {Faker::Hipster.paragraph}
+    activity_id 1
+    user_id 1
   end
 end
