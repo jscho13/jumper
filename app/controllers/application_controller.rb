7c2da8da790ef492b9bc0fe7e3b1b0226a35d107
Joseph Cho <jscho13@gmail.com>
2016-01-06 14:22:08 -0500

    fix hound indentation issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,7 @@ class ApplicationController < ActionController::Base
 
   protected
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) << :username
-    end
-
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
+  end
 end
