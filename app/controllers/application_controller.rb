24796c281d6f4cb1fb2a97b014b35eb961110b5e
Joseph Cho <jscho13@gmail.com>
2016-01-06 12:51:11 -0500

    fix hound indentation issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-    protected
-
+      protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :username
   end
