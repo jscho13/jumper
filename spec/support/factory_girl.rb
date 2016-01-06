02a099c7f51bea454a84a7f45921cf3f7943893e
Joseph Cho <jscho13@gmail.com>
2016-01-06 12:50:37 -0500

    address hound issue

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -5,18 +5,18 @@ FactoryGirl.define do
   factory :user do
     sequence(:username) { |n| "SayWhaaaaatttt#{n}" }
     sequence(:email) { |n| "faketestuser#{n}@example.com" }
-    password {Faker::Internet.password}
+    password { Faker::Internet.password }
   end
 
   factory :venue do
-    venue_name {Faker::Hipster.word}
-    description {Faker::Hipster.paragraph}
+    venue_name { Faker::Hipster.word }
+    description { Faker::Hipster.paragraph }
     price_range "2"
   end
 
   factory :review do
     rating 5
-    review_body {Faker::Hipster.paragraph}
+    review_body { Faker::Hipster.paragraph }
     venue
     user
   end
