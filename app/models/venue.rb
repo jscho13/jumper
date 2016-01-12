5aeaac6a28378fe4be058823178c8938043b7eb2
Mark Fletcher <mook.fletcher@gmail.com>
2016-01-12 11:58:10 -0500

    hound issues 4

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,10 +1,14 @@
 class StateValidator < ActiveModel::Validator
   def validate(record)
-   state_array = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
-   unless state_array.include?(record.state)
-     record.errors[:name] << 'Need a proper state acronym'
-   end
- end
+    STATE_ARRAY = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA",
+                   "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+                   "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+                   "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+                   "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+    unless state_array.include?(record.state)
+      record.errors[:name] << 'Need a proper state acronym'
+    end
+  end
 end
 
 class Venue < ActiveRecord::Base
