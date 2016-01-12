f8925bdca5c7e8cb136692087c65de9f29edc004
Mark Fletcher <mook.fletcher@gmail.com>
2016-01-12 14:40:10 -0500

    addressed hound comments

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,10 +1,10 @@
 class Venue < ActiveRecord::Base
 
-STATES = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA",
-                 "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
-                 "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
-                 "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
-                 "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+  STATES = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL",
+    "GA","HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+    "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM",
+    "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN",
+    "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
 
   validates :venue_name, presence: true
   validates :street_name, presence: true
