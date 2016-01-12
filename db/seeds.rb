d6520d215c67377f8611ee83babb43384e696432
Mark Fletcher <mook.fletcher@gmail.com>
2016-01-12 11:54:03 -0500

    hound fixes 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'faker'
-STATE_ARRAY = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI",
-   "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS",
-   "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR",
-   "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+STATE_ARRAY = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA",
+               "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+               "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+               "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+               "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
 
 User.create(
   username: "a",
--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -1,12 +1,11 @@
 require 'faker'
 FactoryGirl.find_definitions
 
-STATE_ARRAY = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT",
-  "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS",
-  "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO",
-  "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND",
-  "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX",
-  "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+STATE_ARRAY = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA",
+               "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+               "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+               "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+               "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
 
 FactoryGirl.define do
   factory :user do
