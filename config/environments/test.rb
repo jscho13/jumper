4b18555c6bb9c492368be250cd5becf4412cb079
Mark Fletcher <mook.fletcher@gmail.com>
2016-01-13 13:45:28 -0500

    finishing mailing functionality

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
