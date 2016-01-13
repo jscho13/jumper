0f7f0c2cdc7cd2a71dfe48aaf504de888fb09e85
Mark Fletcher <mook.fletcher@gmail.com>
2016-01-13 16:22:13 -0500

    addressed PR comments 2

--- spec/features/email_is_sent_to_user_when_new_review_is_created_spec.rb
+++ spec/features/email_is_sent_to_user_when_new_review_is_created_spec.rb
@@ -1,11 +1,10 @@
 require "rails_helper"
 
 feature "email is sent when review is created" do
-  # let!(:user) { FactoryGirl.create(:user) } self-13
-  # let!(:user_2) { FactoryGirl.create(:user) } self-14
+
   let!(:venue) { FactoryGirl.create(:venue) }
   let!(:review_poster) { FactoryGirl.create(:user) }
-  # let!(:venue_poster) { User.find(venue.user_id) }
+
   scenario "email is sent to the venue creator" do
     sign_in_as(review_poster)
     visit venues_path
