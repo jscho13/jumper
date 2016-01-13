e31069e340a26d6a477ccf3f00171a5c67d8d129
Mark Fletcher <mook.fletcher@gmail.com>
2016-01-13 14:22:47 -0500

    Ajax - fix hound errors

--- app/assets/javascripts/vote.js
+++ app/assets/javascripts/vote.js
@@ -8,8 +8,8 @@ $(function() {
 
     request.done(function(data) {
       var review = $("#review-" + data.id);
-      review.find(".helpful").text("Helpful: " + data.revup_count)
-      review.find(".not-helpful").text("Not Helpful: " + data.revdown_count)
+      review.find(".helpful").text("Helpful: " + data.revup_count);
+      review.find(".not-helpful").text("Not Helpful: " + data.revdown_count);
     });
   };
   var onVoteClick = function(event) {
@@ -20,7 +20,7 @@ $(function() {
       data = { down: "true", load_javascript: "true"};
     } else {
       data = { up: "true", load_javascript: "true" };
-    };
+    }
     var form = reviewButton.find("form");
     var url = form.attr("action");
 
