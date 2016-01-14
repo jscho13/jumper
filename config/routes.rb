58bafece4673cecc0d307ab91893bca75a463d68
Mark Fletcher <mook.fletcher@gmail.com>
2016-01-14 10:28:18 -0500

    idk

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   end
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
 
+  match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
+
   namespace :admin do
     resources :users, only: [:index, :destroy]
   end
