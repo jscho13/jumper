a0a1cafe123dcd66a1915c88a695dd865fa46de4
Mark Fletcher <mook.fletcher@gmail.com>
2016-01-15 13:17:57 -0500

    fixed conflicts

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,7 +30,7 @@ RSpec.configure do |config|
   config.before :each do
     ActionMailer::Base.deliveries.clear
   end
-  
+
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
