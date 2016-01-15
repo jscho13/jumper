ae126e75cf01b381e6ade255978cdd9962174042
Joseph Cho <jscho13@gmail.com>
2016-01-15 11:20:38 -0500

    resolve hound issues 3

--- spec/features/user_cannot_access_admin_functionalities_spec.rb
+++ spec/features/user_cannot_access_admin_functionalities_spec.rb
@@ -2,13 +2,9 @@ require "rails_helper"
 
 feature "user does not have admin access" do
   let!(:review) { FactoryGirl.create(:review) }
-  let!(:review_2) { FactoryGirl.create(:review) }
   let!(:venue) { review.venue }
-  let!(:venue_2) { review_2.venue }
   let!(:user) { venue.user }
-  let!(:user_2) { review.user }
-  let!(:user_3) { venue_2.user }
-  let!(:user_4) { review_2.user }
+  let!(:error_message) { ActionController::RoutingError }
 
   scenario "user cannot access admin functionalities" do
     sign_in_as(user)
@@ -17,7 +13,6 @@ feature "user does not have admin access" do
     expect(page).to_not have_content("Delete Venue")
     expect(page).to_not have_content("Delete Review")
 
-    expect { visit admin_users_path
-    }.to raise_error(ActionController::RoutingError)
+    expect { visit admin_users_path }.to raise_error(error_message)
   end
 end
