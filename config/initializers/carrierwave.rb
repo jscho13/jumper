0cba377257006226c86b7d541d0150380fd58b70
Joseph Cho <jscho13@gmail.com>
2017-04-28 00:59:02 -0400

    use asset pipeline instead of public

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: "AWS",
-    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"] }
-  config.fog_directory = ENV["S3_BUCKET"]
-end
+# CarrierWave.configure do |config|
+#   config.fog_credentials = {
+#     provider: "AWS",
+#     aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+#     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"] }
+#   config.fog_directory = ENV["S3_BUCKET"]
+# end
